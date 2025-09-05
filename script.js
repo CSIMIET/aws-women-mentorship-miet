272c98f82b91ca3a92b62610764f3efb1a52bcb2
Geet <geetanshigoel63@gmail.com>
2025-09-05 22:20:33 +0000

    fix6

--- script.js
+++ script.js
@@ -152,7 +152,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Form submission using the specified approach
-    const form = document.forms['submit-to-google-sheet'];
     form.addEventListener('submit', e => {
         e.preventDefault();
 
